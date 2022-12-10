53ebdec2977da4d7a7369db230831672d7e69588
alessandro ladu <ale1a@libero.it>
2022-12-10 16:56:18 +0000

    implement add ingredient on shopping list component - extra implem of form validation with ng-nativeValidate

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -14,14 +14,18 @@ import { Ingredient } from 'src/app/shared/ingredient.model';
   styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent {
+  name = '';
+  amount = 0;
   @ViewChild('nameInput') nameInputRef: ElementRef;
   @ViewChild('amountInput') amountInputRef: ElementRef;
   @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
-  onAddItem() {
+  onAddItem(nameInput, amountInput) {
     const ingName = this.nameInputRef.nativeElement.value;
     const ingAmount = this.amountInputRef.nativeElement.value;
     const newIngredient = new Ingredient(ingName, ingAmount);
-    this.ingredientAdded.emit(newIngredient);
+    if (this.name.length > 0) this.ingredientAdded.emit(newIngredient);
+    this.name = '';
+    this.amount = 0;
   }
 }
