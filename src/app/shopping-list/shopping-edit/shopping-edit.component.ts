6ccefa9cf0589b471e687736008424c452f3eac0
alessandro ladu <ale1a@libero.it>
2022-12-18 12:33:17 +0000

    removed required from shopping edit forms inputs

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -14,8 +14,6 @@ import { Ingredient } from 'src/app/shared/ingredient.model';
   styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent {
-  name = '';
-  amount = 0;
   @ViewChild('nameInput') nameInputRef: ElementRef;
   @ViewChild('amountInput') amountInputRef: ElementRef;
   @Output() ingredientAdded = new EventEmitter<Ingredient>();
@@ -24,8 +22,6 @@ export class ShoppingEditComponent {
     const ingName = this.nameInputRef.nativeElement.value;
     const ingAmount = this.amountInputRef.nativeElement.value;
     const newIngredient = new Ingredient(ingName, ingAmount);
-    if (this.name.length > 0) this.ingredientAdded.emit(newIngredient);
-    this.name = '';
-    this.amount = 0;
+    if (ingName.length > 0) this.ingredientAdded.emit(newIngredient);
   }
 }
